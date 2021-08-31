a324265ed4863166a9b7abc84c38b7c0c863d33e
bexi-91 <u3207389@uni.canberra.edu.au>
2021-08-31 14:19:50 +1000

    sql

--- Public/assets/works.sql
+++ Public/assets/works.sql
@@ -5,3 +5,11 @@ CREATE TABLE `works` (
   `duedate` date DEFAULT NULL,
   `taskdetails` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
